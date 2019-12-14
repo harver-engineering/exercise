8d081933f5e2bb78ff4d5f017fcf1b26640552b4
Dhammika Marasinghe <dhammikamare@gmail.com>
2019-12-14 21:01:13 +0530

    Print random words

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const { getRandomWordSync, getRandomWord } = require('word-maker');
 
-console.log('It works!');
+// create numbers from 1 to 100
+const ids = Array.from(Array(100).keys()).map( id => id + 1);
 
-// YOUR CODE HERE
+/**
+ * Task 1
+ * Print numbers from 1 to 100 to the console, but for each number also print a random word
+ */
+console.info("\n*** Task 1 ***\n");
+
+const randomWords = (id) => id + ": " + getRandomWordSync();
+ids.map(id => console.log(randomWords(id)));
