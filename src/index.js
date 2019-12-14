ed5f72e8f8c9cba547e860c16c3a0b7fa9d73227
Dhammika Marasinghe <dhammikamare@gmail.com>
2019-12-14 21:40:23 +0530

    Implement async solutions

--- src/index.js
+++ src/index.js
@@ -23,3 +23,20 @@ const doFizzBuzz = (id) => (id%3 === 0 ? "Fizz" : "") + (id%5 === 0 ? "Buzz" : "
 
 const fizzBuzzRandomWords = (id) => id + ": " + (doFizzBuzz(id) || getRandomWordSync());
 ids.map(id => console.log(fizzBuzzRandomWords(id)));
+
+/**
+ * Task 3
+ * Asynchronous Implementations of Task 1 & 2
+ */
+
+// 3.1 Async Random Words
+
+const randomWordsAsync = async (id) => id + ": " + await getRandomWord();
+let promises = ids.map(id => randomWordsAsync(id));
+Promise.all(promises).then(output => console.log(output.join('\n')));
+
+// 3.2 Async Random words with "Fizz Buzz" program
+
+const fizzBuzzRandomWordsAsync = async (id) => id + ": " + (doFizzBuzz(id) || await getRandomWord());
+let promises2 = ids.map(id => fizzBuzzRandomWordsAsync(id));
+Promise.all(promises2).then(output => console.log(output.join('\n')));
